78649a5b609c9035af732c639d466649e823e056
Tristian Kelly <tristian.kelly560@gmail.com>
2018-02-07 15:03:43 -0600

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -62,11 +62,11 @@ globP('**.@(md|html)', { cwd: `content` })
           // render layout with page contents
           const layout = pageData.attributes.layout || 'default'
 
-          return ejsRenderFile(pug.renderFile(`views/${layout}.html`), Object.assign({}, templateConfig, { content: pageContent }))
+          return ejsRenderFile(`views/${layout}.html`, Object.assign({}, templateConfig, { content: pageContent }))
         })
         .then((str) => {
           // save the html file
-          fse.writeFile(`${destPath}/${fileData.name}.html`, str)
+          fse.writeFile(pug.render(`${destPath}/${fileData.name}.html`, str))
       })
         .catch((err) => { console.error(err) })
     })
