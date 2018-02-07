8559c9833f0f5af0d01a141e07c17f104a9222b1
Tristian Kelly <tristian.kelly560@gmail.com>
2018-02-07 14:18:57 -0600

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -20,7 +20,7 @@ const frontMatter = require('front-matter')
 const globP = promisify(require('glob'))
 const config = require('../site.config')
 
-const ejsRenderFile = promisify(ejs.renderFile)
+const ejsRenderFile = promisify(pug.render promisify(ejs.renderFile))
 const distPath = './site'
 
 // set ejs delimiter
@@ -63,7 +63,7 @@ globP('**.@(md|html)', { cwd: `content` })
           // render layout with page contents
           const layout = pageData.attributes.layout || 'default'
 
-          return pug.render(ejsRenderFile(`views/${layout}.html`, Object.assign({}, templateConfig, { content: pageContent })))
+          return ejsRenderFile(`views/${layout}.html`, Object.assign({}, templateConfig, { content: pageContent }))
         })
         .then((str) => {
           // save the html file
